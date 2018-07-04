b35a4b1558b567c9adf54a46f5e154508d83a1c7
allison <allielu.allison@gmail.com>
2018-07-03 22:48:56 -0500

    integrate redux with grid

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import StackGrid, { transitions, easings } from "react-stack-grid";
 
 import ExampleComponent from './components/example-component'
-import Grid from './components/grid'
 import Center from 'react-center';
 
+const { scaleDown } = transitions;
+
 class App extends Component {
-  exampleComponents = [1, 2, 3, 4, 5]
+  exampleComponents = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
   render() {
-
     var exampleComponents = this.exampleComponents.map(function(exampleNum) {
     return <ExampleComponent useThisToPassDataBetweenComponentsExample={exampleNum} key={exampleNum}/>
   })
@@ -24,8 +25,22 @@ class App extends Component {
         <p className="App-intro">
           Welcome to Creative Prompts! Post your own response to the prompt above or find some inspiration from others{"\'"} posts.
         </p>
+        <br />
+
+        <StackGrid
+          columnWidth={350}
+          gutterWidth={35}
+          gutterHeight={35}
+          duration={1000}
+          appear={scaleDown.appear}
+          appeared={scaleDown.appeared}
+          enter={scaleDown.enter}
+          entered={scaleDown.entered}
+          leaved={scaleDown.leaved}
+          //monitorImagesLoaded={true}
+        >
         {exampleComponents}
-        <Grid/>
+        </StackGrid>
       </div>
     );
   }
--- src/components/example-component.js
+++ src/components/example-component.js
@@ -4,7 +4,6 @@ import {connect} from 'react-redux';
 import * as exampleActions from '../actions/exampleActions';
 import * as actionTypes from '../actions/actionTypes'
 
-
 class ExampleComponent extends Component {
 	constructor(props) {
 		super(props);
@@ -21,6 +20,8 @@ class ExampleComponent extends Component {
 		return (
 			<div className="example-component">
 				<p>Hi, I am example component number {exampleNum} and the example count from store is {this.props.countExample}</p>
+				<p>Wow, isn{"\'"}t this cool? Now we have integrated the content {"with"} the redux. </p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. At quis risus sed vulputate odio ut. Orci sagittis eu volutpat odio blah blah blah blah blah</p>
 				<button onClick={this.addCountExampleInStore}>click</button>
 			</div>
 		);
