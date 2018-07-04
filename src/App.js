a76af725016e947ef43fb0eb06c760e0814ddf99
allison <allielu.allison@gmail.com>
2018-07-03 19:25:48 -0500

    add a grid thingy

--- src/App.js
+++ src/App.js
@@ -3,24 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 
 import ExampleComponent from './components/example-component'
+import Grid from './components/grid'
+import Center from 'react-center';
 
 class App extends Component {
-  exampleComponents = [1, 2, 3, 4, 5]
+  //exampleComponents = [1, 2, 3, 4, 5]
 
   render() {
-    var exampleComponents = this.exampleComponents.map(function(exampleNum) {
-      return <ExampleComponent useThisToPassDataBetweenComponentsExample={exampleNum}/>
-    })
+    // var exampleComponents = this.exampleComponents.map(function(exampleNum) {
+    //   return <ExampleComponent useThisToPassDataBetweenComponentsExample={exampleNum}/>
+    // })
+    // var grid = this.function(){
+    //   return <Grid/>
+    // }s
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Creative Prompts!!</h1>
+          <h1 className="App-title">Succulents</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Welcome to Creative Prompts! Post your own response to the prompt above or find some inspiration from others{"\'"} posts.
         </p>
-        {exampleComponents}
+        <Grid/>
       </div>
     );
   }
