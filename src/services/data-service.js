6bf29cf4cef53df6310deb426099cb8e89eb2816
Allison <allison@allisons-mbp.attlocal.net>
2018-08-08 23:42:44 -0500

    prompts update at 5am est

--- src/services/data-service.js
+++ src/services/data-service.js
@@ -2,6 +2,7 @@ import { Post } from '../models/post';
 
 import firebaseReference from "../firebaseConfig";
 import 'firebase/firestore';
+import { decodedTextSpanIntersectsWith } from '../../node_modules/typescript';
 
 export class DataService {
 	constructor() {
@@ -9,11 +10,13 @@ export class DataService {
 
   getPromptsFromDB(state) {
 
-		 var todayDate = new Date(Date.now());
-		 console.log(todayDate);
+		var now = new Date();
+		var today4am_ms = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 4, 0, 0, 0).getTime();
+		var tomorrow4am_ms = today4am_ms + 86400000;
 
       firebaseReference.collection('days')
-      .where("date", "==" , todayDate.toDateString().substring(4,15))
+			.where("start_time", "<=", Date.now())
+			.where("end_time", "==", tomorrow4am_ms)
       .get()
         .then(snapshot => {
           snapshot.forEach(doc => {
@@ -33,12 +36,12 @@ export class DataService {
   //sets the prompt's state to the same one as DB
   displayPromptsFromDB(prompt, state) {
     state.setState({
-      title: prompt
-    });
+			title: prompt,
+		});
   }
 
-  updatePromptsFromDB(state) {
-    this.getPromptsFromDB(state); //updates state
+  updatePromptsFromDB(state, state_title) {
+		this.getPromptsFromDB(state); //gets the new one
   }
 		
 	getPostsFromDB() {
